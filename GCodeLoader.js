a5c23b3806be02fbe81545eb1960a945cbcf9818
Piotr Majewski <emplast@wp.pl>
2022-02-07 17:23:53 +0100

    Add files via upload

--- GCodeLoader.js
+++ GCodeLoader.js
@@ -68,7 +68,7 @@ class GCodeLoader extends Loader {
 
 	parse( data ) {
 
-		let state = { x: 0, y: 0, z: 0, e: 0, f: 0, extruding: false, relative: false };
+		let state = { x: 0, y: 0, z: 0, e: 0, f: 0,i:0,j:0, extruding: false, relative: false };
 		const layers = [];
 
 		let currentLayer = undefined;
@@ -120,43 +120,17 @@ class GCodeLoader extends Loader {
 			return state.relative ? v1 + v2 : v2;
 
 		}
-		function toRadians(angle) {
-			return angle * (Math.PI / 180);
-		}
 		
-		function toDegrees(angle) {
-			return angle * (180 / Math.PI);
-		}
-		function angleStart(s,a){
-			return 0;
-		}
-		function angleEnd(s,a){
-			var e= new THREE.Vector3(s.x,s.y,0);
-			var c= new THREE.Vector3((a.i+s.x),(a.j+s.y),0);
-			var p= new THREE.Vector3(a.x,a.y,0);
-			var d=c.distanceTo(e);//R
-			var d1=e.distanceTo(p);//D
-			return ((Math.PI*2)-(Math.sin((((0.5*d1)/d)))*2))
-			// console.log(d);
-		}
-		function angleEndT(s,a){
-			var e= new THREE.Vector3(s.x,s.y,0);
-			var c= new THREE.Vector3((a.i+s.x),(a.j+s.y),0);
-			var p= new THREE.Vector3(a.x,a.y,0);
-			var d=c.distanceTo(e);//R
-			var d1=e.distanceTo(p);//D
-			return Math.sin((((0.5*d1)/d)))*2
-			// console.log(d);
-		}
+		let cmd;
 		const lines = data.replace( /;.+/g, '' ).split( '\n' );
 		for ( let i = 0; i < lines.length; i ++ ) {
 
 			const tokens = lines[ i ].split( ' ' );
-			const cmd = tokens[ 0 ].toUpperCase();
+			cmd = tokens[ 0 ].toUpperCase();
 
 			//Argumments
 			const args = {};
-			tokens.splice( 1 ).forEach( function ( token ) {
+			tokens.splice( 0 ).forEach( function ( token ) {
 
 				if ( token[ 0 ] !== undefined ) {
 
@@ -167,10 +141,10 @@ class GCodeLoader extends Loader {
 				}
 
 			} );
-			
+			console.log(cmd)
 			//Process commands
 			//G0/G1 – Linear Movement
-			if (cmd === 'G00' || cmd === 'G0' || cmd === 'G01' || cmd==='G1') {
+			if (cmd === 'G00' || cmd === 'G0' || cmd === 'G01' || cmd==='G1' || /(([XxYyZz]) *(-?\d+.?\d*)) *(([XxYyZz]) *(-?\d+.?\d*))? *(([XxYyZz]) *(-?\d+.?\d*))?/g.test(cmd)) {
 
 				const line = {
 					x: args.x !== undefined ? absolute( state.x, args.x ) : state.x,
@@ -195,53 +169,34 @@ class GCodeLoader extends Loader {
 
 				addSegment( state, line );
 				state = line;
-				console.log(state)
 			} else if ( cmd==='G02' || cmd === 'G2' || cmd==='G03' || cmd === 'G3' ) {
 
-				const line={
-				    x:args.x,
-					y:args.y,
-					z:args.z,
-					i:args.i,
-					j:args.j,
-					f:state.f,
-					e:state.e,
-					a:(state.a===undefined)?0:state.a+angleEnd(state,args),
-					b:(state.b===undefined)?0:state.b+angleEnd(state,args)
-					}
-				var	s=new THREE.Vector3(state.x,state.y,0),
-					e=new THREE.Vector3(state.x+line.i,state.y+line.j,0),
-					r=s.distanceTo(e);
+					const line = {
+						x: args.x !== undefined ? absolute( state.x, args.x ) : state.x,
+						y: args.y !== undefined ? absolute( state.y, args.y ) : state.y,
+						z: args.z !== undefined ? absolute( state.z, args.z ) : state.z,
+						i: args.i !== undefined ? absolute( state.i, args.i ) : state.i,
+					    j: args.j !== undefined ? absolute( state.j, args.j ) : state.j,
+						e: args.e !== undefined ? absolute( state.e, args.e ) : state.e,
+						f: args.f !== undefined ? absolute( state.f, args.f ) : state.f,
+					};
 					
-					console.log(r);
-					(cmd=='G03' || cmd=='G3')?line.b=0:line.a=0;
-					
-				const arc=new THREE.EllipseCurve(
-					line.i+state.x,
-					line.j+state.y,
-					r,
-					r,
-					(cmd==='G3' || cmd==='G03')?Math.PI-angleEnd(state,args)-line.a:Math.PI+line.b,
-					(cmd==='G3' || cmd==='G03')?Math.PI-line.a:Math.PI+line.b-angleEnd(state,args),
-					// 0,
-					(cmd==='G3' || cmd==='G03')?true:false,
-					0
-					
+					if ( delta( state.e, line.e ) > 0 ) {
 
-				)
-				var points = arc.getPoints( 50 );
-				const geometry = new THREE.BufferGeometry().setFromPoints( points );
-				const material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-				const ellipse = new THREE.Line( geometry, material );
-				ellipse.quaternion.setFromEuler( new Euler( - Math.PI / 2, 0, 0 ) );
-				ellipse.position.set(-100, -50, -300);
-				ellipse.rotateX(toRadians(90));
-				scene.add(ellipse)
-				// if(i<2)
-				//   scene.add(ellipse);
-				// i++;
+						state.extruding = delta( state.e, line.e ) > 0;
+	
+						if ( currentLayer == undefined || line.z != currentLayer.z ) {
+	
+							newLayer( line );
+	
+						}
+	
+					}
+
+
+
+				addSegment(state,line)
 				state=line
-				console.log(line);
 					
 			} else if ( cmd === 'G90' ) {
 
@@ -271,13 +226,29 @@ class GCodeLoader extends Loader {
 
 		}
 
-		function addObject( vertex, extruding, i ) {
+		function addObject( vertex, extruding, i ,cmd) {
+			
+			if(cmd!='G02' || cmd != 'G03' || cmd !='G2' || cmd != 'G3'){
 
-			const geometry = new BufferGeometry();
-			geometry.setAttribute( 'position', new Float32BufferAttribute( vertex, 3 ) );
-			const segments = new LineSegments( geometry, extruding ? extrudingMaterial : pathMaterial );
-			segments.name = 'layer' + i;
-			object.add( segments );
+				const geometry = new BufferGeometry();
+				geometry.setAttribute( 'position', new Float32BufferAttribute( vertex, 3 ) );
+				const segments = new LineSegments( geometry, extruding ? extrudingMaterial : pathMaterial );
+				segments.name = 'layer' + i;
+				object.add( segments );
+
+			}else{
+				const curve = new THREE.SplineCurve( [
+					new THREE.Vector2( state.x, state.y ),
+					new THREE.Vector2( args.x,args.y )
+					
+				] );
+				
+				const points = curve.getPoints( 50 );
+				const geometry = new THREE.BufferGeometry().setFromPoints( points );
+				const ellipse = new THREE.Line( geometry, extruding ? extrudingMaterial : pathMaterial );
+				ellipse.name = 'layer' + i;
+				object.add(ellipse);
+			}
 
 		}
 
@@ -289,8 +260,8 @@ class GCodeLoader extends Loader {
 			for ( let i = 0; i < layers.length; i ++ ) {
 
 				const layer = layers[ i ];
-				addObject( layer.vertex, true, i );
-				addObject( layer.pathVertex, false, i );
+				addObject( layer.vertex, true, i ,cmd);
+				addObject( layer.pathVertex, false, i ,cmd);
 
 			}
 
@@ -319,8 +290,8 @@ class GCodeLoader extends Loader {
 
 			}
 
-			addObject( vertex, true, layers.length );
-			addObject( pathVertex, false, layers.length );
+			addObject( vertex, true, layers.length ,cmd);
+			addObject( pathVertex, false, layers.length ,cmd);
 
 		}
 
